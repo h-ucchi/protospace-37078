658781926a554a0c036a67615d380a409cad6681
h-ucchi <h-ucchi+study@github.com>
2022-02-06 15:17:25 +0900

    ログアウト時の遷移先変更
    ログインしていない状態で、新規投稿ページに遷移した場合に、ユーザーログインページに飛ぶようにした

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -53,7 +53,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to root_path
+      redirect_to user_session_path
     end
   end
 end
